7a73ace25a7bc2c077644eada2860f2c7f68293d
phil <jaeja.phil@gmail.com>
2024-03-24 19:19:01 +0900

    first commit

---
+++ src/main/kotlin/com/example/hhplustddweek2/HhplusTddWeek2Application.kt
@@ -0,0 +1,11 @@
+package com.example.hhplustddweek2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HhplusTddWeek2Application
+
+fun main(args: Array<String>) {
+    runApplication<HhplusTddWeek2Application>(*args)
+}
---
+++ src/test/kotlin/com/example/hhplustddweek2/HhplusTddWeek2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.hhplustddweek2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HhplusTddWeek2ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
