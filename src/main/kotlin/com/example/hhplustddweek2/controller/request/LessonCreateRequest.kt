1e4b776f93d03896876b6c663b8f219bde6a87a8
phil <jaeja.phil@gmail.com>
2024-03-25 01:24:26 +0900

    feat(Lesson): add missed LessonCreateRequest

---
+++ src/main/kotlin/com/example/hhplustddweek2/controller/request/LessonCreateRequest.kt
@@ -0,0 +1,9 @@
+package com.example.hhplustddweek2.controller.request
+
+import java.time.LocalDateTime
+
+data class LessonCreateRequest(
+    val name: String,
+    val description: String,
+    val lessonDate: LocalDateTime
+)
