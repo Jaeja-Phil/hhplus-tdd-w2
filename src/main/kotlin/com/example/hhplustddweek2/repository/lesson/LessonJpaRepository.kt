330797542d4b100b2734944a2a589c0c414f6149
phil <jaeja.phil@gmail.com>
2024-03-25 19:33:48 +0900

    feat(Lesson): add a query with write-lock
    - create an interface to use when update is a potential outcome

--- src/main/kotlin/com/example/hhplustddweek2/repository/lesson/LessonJpaRepository.kt
+++ src/main/kotlin/com/example/hhplustddweek2/repository/lesson/LessonJpaRepository.kt
@@ -1,8 +1,16 @@
 package com.example.hhplustddweek2.repository.lesson
 
 import com.example.hhplustddweek2.entity.LessonEntity
+import jakarta.persistence.LockModeType
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Lock
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
-interface LessonJpaRepository : JpaRepository<LessonEntity, Long>
+interface LessonJpaRepository : JpaRepository<LessonEntity, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT le FROM LessonEntity le WHERE le.id = :id")
+    fun findLessonByIdWithLock(id: Long): Optional<LessonEntity>
+}
